3b1a2b297dcd5ea2ac70b3191cc375c72f685c00
wujianfeng <wujianfeng@myhexin.com>
2020-09-08 20:14:23 +0800

    loadsh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo '*********************start************************'
 export PROJ_PATH=/var/jenkins_home/workspace/test2
+npm install
 pm2 stop ./server.js
 pm2 start ./server.js
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http')
-// var _ = require('lodash');
+var _ = require('lodash')
 http.createServer((req, res) => {
     res.writeHead(200, {
         'content-type': 'text/plain'
     })
-    const str = 'hw random' //+_.random(15, 20)
+    const str = 'hw random' + _.random(15, 20)
     res.write(str)
     res.end()
 }).listen(3333)
