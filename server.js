b72cc85cfac3e9f52205285120daec28e1b9faea
wujianfeng <wujianfeng@myhexin.com>
2020-09-08 21:06:06 +0800

    tag

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ http.createServer((req, res) => {
     res.writeHead(200, {
         'content-type': 'text/plain'
     })
-    const str = 'hw random' + _.random(15, 20)
+    const str = 'tag hw random' + _.random(15, 20)
     res.write(str)
     res.end()
 }).listen(3333)
